24c9a1c2bc60df5e5930b70d8b790f44be06f8a9
Lucas Fidelis <luckas_jf@hotmail.com>
2024-06-27 20:05:30 -0300

    desafio exercicio array

---
+++ curso da logica/exercicioCurso2 array/app.js
@@ -0,0 +1,14 @@
+//Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+//Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python', 'Java', 'Ruby' , 'GoLang'];
+
+//Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+console.log(linguagensDeProgramacao[0]);
+
+//Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(linguagensDeProgramacao[1]);
+
+//Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log((linguagensDeProgramacao[linguagensDeProgramacao.length -1]));
